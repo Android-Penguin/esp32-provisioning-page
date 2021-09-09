4bc6025d8645619a38abee9be33a7630370e80dc
Android-Penguin <49669409+Android-Penguin@users.noreply.github.com>
2021-09-10 09:59:53 +1200

    Added post request
    - Changed order of updating WiFi settings so that settings are only changed where needed.
    
    - Post request to web server for sending configured JSON object.
    
    - Minor modification to comments in file.
    
    - Minified HTML and JS files to keep them within 4KB file limit.
    
    - Created distribution folder for final files.

--- scripts.js
+++ scripts.js
@@ -1,4 +1,4 @@
-// Wifi details
+/*Wifi details*/
 var wifiDetails = document.getElementById("wifi-details");
 function wifiBox(radioButton) {
     if(radioButton.value=="useWifi") {
@@ -12,7 +12,7 @@ function wifiBox(radioButton) {
     }
 }
 
-// Static IP config
+/*Static IP config*/
 var staticIPBox = document.getElementById("static-ip-box");
 function staticConfigBox(radioButton) {
     if(radioButton.value!="useDHCP") {
@@ -25,8 +25,7 @@ function staticConfigBox(radioButton) {
         staticIPBox.style.borderStyle = "none";
     }
 }
-
-// Update values
+/*Update values*/
 function updateValues() {
     for (item in networkConfig) {
         if(item == "hasEthernet") {
@@ -54,7 +53,7 @@ function updateValues() {
             if(document.getElementById(item) !== null) {
                 document.getElementById(item).value = networkConfig[item];
                 if(networkConfig[item] !== "") {
-                    document.getElementById("wifiPassword").placeholder = "(unchanged)"
+                    document.getElementById("wifiPassword").placeholder = "(unchanged)";
                 }
             }
         } else if(item == "wifiPassword") {
@@ -68,14 +67,13 @@ function updateValues() {
         }
     }
 }
-
-// Submit form
+/*Submit form*/
 document.getElementById("confirm").addEventListener("click", function(event){
     event.preventDefault();
     for (item in networkConfig) {
         if(item == "hasEthernet") {
 
-        } else if(item == "useWifi" || item == "useEthernet" || item == "useDHCP") {
+        } else if(item == "useEthernet" || item == "useDHCP") {
             if(document.getElementById(item) !== null) {
                 if(document.getElementById(item).checked == true) {
                     networkConfig[item] = 1;
@@ -83,39 +81,65 @@ document.getElementById("confirm").addEventListener("click", function(event){
                     networkConfig[item] = 0;
                 }
             }
-        } else if(item == "wifiSSID") {
+        } else if(item == "useWifi") {
             if(document.getElementById(item) !== null) {
-                if(networkConfig[item] != document.getElementById(item).value) {
-                    networkConfig[item] = document.getElementById(item).value;
+                if(document.getElementById(item).checked == true) {
+                    networkConfig[item] = 1;
+                    /*Update wifi SSID if wifi is true*/
+                    if(document.getElementById("wifiSSID") !== null) {
+                        console.log(networkConfig["wifiSSID"]);
+                        if(networkConfig["wifiSSID"] != document.getElementById("wifiSSID").value) {
+                            networkConfig["wifiSSID"] = document.getElementById("wifiSSID").value;
+                        }
+                        console.log(networkConfig["wifiSSID"]);
+                    }
+                } else {
+                    networkConfig[item] = 0;
                 }
             }
-        } else if(item == "wifiPassword") {
-            if(document.getElementById(item) !== null) {
-                console.log(networkConfig[item]);
-                if(document.getElementById(item).value == "") {
-                    delete networkConfig[item];
+            /*Update password regardless of wifi settings*/
+            if(document.getElementById("wifiPassword") !== null) {
+                console.log(networkConfig["wifiPassword"]);
+                if(document.getElementById("wifiPassword").value == "") {
+                    delete networkConfig["wifiPassword"];
                     console.log("item deleted");
                 } else {
                     networkConfig[item] = document.getElementById(item).value;
                     console.log("item modified");
+                    networkConfig["wifiPassword"] = document.getElementById("wifiPassword").value;
                 }
-                console.log(networkConfig[item]);
+                console.log(networkConfig["wifiPassword"]);
             }
+        } else if(item == "wifiSSID") {
+        } else if(item == "wifiPassword") {
         } else {
             if(document.getElementById(item) !== null) {
                 networkConfig[item] = document.getElementById(item).value;
             }
         }
     }
-    // TODO - will all fields alway be in object? Script assumes that they are there and sets them accordingly.
-});
+    return fetch(window.location.href + '/writeConfig', {
+            method: 'post',
+            body: JSON.stringify(networkConfig),
+        })
+        .then(function (response) {
+            console.log('writeConfig response:', response);
+        })
+        .then(function (responseData) {
+            console.log('writeConfig response data:', JSON.stringify(responseData));                
+        })
+        .catch(function (error) {
+            console.log('writeConfig response error:', JSON.stringify(error));
+        });
+    }
+);
 
-// Cancel changes
+/*Cancel changes*/
 document.getElementById("cancel").addEventListener("click", function(event){
     event.preventDefault();
 });
 
-// Prevent enter key
+/*Prevent enter key*/
 var textFields = document.getElementsByClassName("text-field");
 for(item of textFields) {
     item.addEventListener('keypress',enter_detector,false);
